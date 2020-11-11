d15c45f41988753942b8bbf133c8378f4c149edb
it_dog <it_dog_zhang@163.com>
2020-11-11 22:12:25 +0800

    集成druid

--- src/main/java/com/dog/it/aspect/LogAspect.java
+++ src/main/java/com/dog/it/aspect/LogAspect.java
@@ -10,40 +10,40 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class LogAspect {
-    @Pointcut("execution(* com.dog.it.service.*.*(..))")
-    public void pc1(){
-
-    }
-
-    @Before(value = "pc1()")
-    public void before(JoinPoint joinPoint){
-        log.info(joinPoint.getSignature().getName());
-        log.info("开始执行");
-    }
-
-    @After(value = "pc1()")
-    public void after(JoinPoint joinPoint){
-        log.info(joinPoint.getSignature().getName());
-        log.info("执行完毕");
-    }
-
-    @AfterReturning(value = "pc1()",returning = "result")
-    public void afterReturn(JoinPoint joinPoint,Object result){
-        log.info(joinPoint.getSignature().getName());
-        log.info("返回了:");
-        log.info(result.toString());
-    }
-
-    @AfterThrowing(value = "pc1()",throwing = "e")
-    public void afterThrow(JoinPoint joinPoint,Exception e){
-        log.info(joinPoint.getSignature().getName());
-        log.info("抛异常了:");
-        log.info(e.getMessage());
-    }
-
-    @Around(value = "pc1()")
-    public Object afterThrow(ProceedingJoinPoint pip) throws Throwable {
-        return pip.proceed();
-    }
+//    @Pointcut("execution(* com.dog.it.service.*.*(..))")
+//    public void pc1(){
+//
+//    }
+//
+//    @Before(value = "pc1()")
+//    public void before(JoinPoint joinPoint){
+//        log.info(joinPoint.getSignature().getName());
+//        log.info("开始执行");
+//    }
+//
+//    @After(value = "pc1()")
+//    public void after(JoinPoint joinPoint){
+//        log.info(joinPoint.getSignature().getName());
+//        log.info("执行完毕");
+//    }
+//
+//    @AfterReturning(value = "pc1()",returning = "result")
+//    public void afterReturn(JoinPoint joinPoint,Object result){
+//        log.info(joinPoint.getSignature().getName());
+//        log.info("返回了:");
+//        log.info(result.toString());
+//    }
+//
+//    @AfterThrowing(value = "pc1()",throwing = "e")
+//    public void afterThrow(JoinPoint joinPoint,Exception e){
+//        log.info(joinPoint.getSignature().getName());
+//        log.info("抛异常了:");
+//        log.info(e.getMessage());
+//    }
+//
+//    @Around(value = "pc1()")
+//    public Object afterThrow(ProceedingJoinPoint pip) throws Throwable {
+//        return pip.proceed();
+//    }
 
 }
